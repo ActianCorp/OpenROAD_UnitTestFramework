608ab5cad977f9917a788c17e5e0b7a0eb3fe993
clach04 <Chris.Clark@actian.com>
2015-10-10 06:24:25 -0700

    Initial basic framework and most simple demo

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# OpenROAD_UnitTestFramework
-OpenROAD unittest framework
+# OpenROAD UnitTest Framework
+
+Needs:
+
+  * OpenROAD 6.2 (due to renameapp requirement)
+  * Cygwin (from https://www.cygwin.com/)
+      * bash (some bash extensions have been used)
+      * diffutils (diff)
+  * Some tests expect a hard coded temporary path location needs to exist:
+      * Windows; `C:\temp`
+      * Unix; `/tmp`
+
+Sample usage:
+
+    # Windows/Cygwin specific
+    # Ensure Cygwin is in the path, assuming default location for 32-bit or 64-bit:
+    path C:\cygwin\bin;%PATH%
+    path C:\cygwin64\bin;%PATH%
+
+    # allow Windows new lines in shell scripts
+    set SHELLOPTS=igncr
+
+    md C:\temp
+
+    destroydb -uingres orunit
+    createdb -uingres orunit
+    bash or_tests.bash orunit
+    #bash or_tests.bash localXI::orunit
+
+## Instructions
+
+  1. Create a new application:
+       1. Include `unittests`    FIXME rename, `UnitTestFramework`.
+  2. Create new Userclass:
+       1. Inherit from `testcase`.
+       2. Create Method in Userclass editor, that starts with the word `test` (NOTE lowercase).
+       3. Create Method code in script editor.
+  3. Create 4gl procedure named `runtests` (or import from another test)
+      1. Ensure `runtests` calls Userclass name defined from above
+      2. Ensure `runtests` is the starting component for the application
+
---
+++ or_tests.bash
@@ -0,0 +1,161 @@
+#!/bin/bash
+#
+# This script tests OpenROAD functionality.
+#   It has to be run in an OpenROAD environment (II_SYSTEM, PATH, etc. should be set)
+#   Ingres/Net and OR Server should  be running.
+#   The parameter passed is the database (incl. vnode) used for the tests.
+# USAGE:
+#        or_tests.bash <dbname>
+#
+
+# -----------------------------------------------------------------------------
+# Function TEST_CLEANUP
+#   This function performs the cleanup (deletion of files) and exit of the script.
+#   Parameter passed is the exit code to be returned to the shell.
+# ------------------------------------------------------------------------------
+TEST_CLEANUP()
+{
+    return_code=$1
+    if [ -d $TESTDIR ]
+    then
+        cd $TESTDIR
+        rm -f *.xml *.img
+    fi
+    exit $return_code
+}
+
+TEST_CHECKCMD()
+{
+# -----------------------------------------------------------------------------
+# Function TEST_CHECKCMD
+#   First parameter passed is the return code of the last command executed.
+#   Second parameter passed is the expected return code.
+#   Third parameter:
+#       Y: Indicates an error is deemed as critical
+#       N: Any errors are reported, but not as critical
+#   Remaining parameters give a brief description of what the command was doing.
+# ------------------------------------------------------------------------------
+   h_clf_return_code=$1
+   shift
+
+   h_clf_return_code_expected=$1
+   shift
+
+   h_clf_critical=$1
+   shift
+
+   h_clf_command=$*
+
+   if [ $h_clf_return_code -ne $h_clf_return_code_expected ]
+   then
+      printf "++ ERROR ++\n%s\n" "$h_clf_command"
+      printf "+++ Status Code: expected: %s, actual: %s\n\n" $h_clf_return_code_expected $h_clf_return_code
+
+      if [ "$h_clf_critical" = "Y" ]
+      then
+         if [ -f $TESTDIR/test.log ]
+         then
+            printf "\nw4gl log file:\n\n"
+            cat $TESTDIR/test.log
+         fi
+         TEST_CLEANUP 1
+      else
+          ((rv++))
+      fi
+   fi
+
+   return 0
+}
+
+# -----------------------------------------------------------------------------
+#       Main execution
+# -----------------------------------------------------------------------------
+
+rv=0
+
+cd $(dirname $0)
+export SCRIPTDIR=`pwd`
+
+if [ $# -lt 1 ]
+then
+    printf "USAGE:\n\t$0 <dbname> [<testno>]\n\n"
+    exit 1
+fi
+if [ -z "$1" ]
+then
+    printf "Empty <dbname> supplied!\n\n"
+    exit 1
+fi
+export TESTDB=$1
+if [ -z "$2" ]
+then
+    export TESTNO=$(date +"%y%m%d%H%M%S")
+else
+    export TESTNO=$2
+fi
+
+export TESTDIR=$SCRIPTDIR/tests/test${TESTNO}
+
+echo '\q' | tm -S $TESTDB
+TEST_CHECKCMD $? 0 "Y" "Unable to connect to ${TESTDB}"
+
+chmod 644 *.xml unittests/*.xml
+mkdir -p $TESTDIR
+TEST_CHECKCMD $? 0 "Y" "Unable to create test directory $TESTDIR"
+cp $SCRIPTDIR/*.xml $TESTDIR
+TEST_CHECKCMD $? 0 "Y" "Unable to copy XML files into test directory $TESTDIR"
+cd $TESTDIR
+TEST_CHECKCMD $? 0 "Y" "Unable to change to test directory $TESTDIR"
+
+export II_LOG=.
+export II_W4GL_EXPORT_INDENTED=TRUE
+unset II_W4GL_EXPORT_COMMENT
+
+
+printf "\nOR Unit tests:\n Using logfile $TESTDIR/orunittest.log ...\n\n"
+
+rm -f *.xml
+cp ${SCRIPTDIR}/unittests/*.xml .
+TEST_CHECKCMD $? 0 "Y" "Unable to copy unittests XML files into test directory $TESTDIR"
+
+rc=0
+
+w4gldev backupapp in ${TESTDB} unittests unittests.xml -nreplace -xml -nowindows -Lorunittest.log -Tyes,logonly
+if [ $? -ne 0 ]
+then
+    printf "\nUnable to import application unittests into ${TESTDB}.\n"
+    TEST_CLEANUP 1
+fi
+
+for utxml in $(ls *.xml | grep -v "^unittests\.xml")
+do
+    utapp=$(basename $utxml .xml)
+    w4gldev backupapp in ${TESTDB} $utapp $utxml -nreplace -xml -nowindows -Lorunittest.log -Tyes,logonly -A
+    rv1=$?
+    if [ $rv1 -eq 0 ]
+    then
+        printf " ${utapp}: ... "
+        w4gldev rundbapp ${TESTDB} $utapp -nowindows -Lorunittest.log -Tyes,logonly -A
+        if [ $? -eq 0 ]
+        then
+            printf "OK.\n"
+        else
+            printf "FAILED.\n"
+            ((rc++))
+        fi
+    else
+        printf " ${utapp}: Import of application FAILED.\n"
+        ((rc++))
+    fi
+done
+
+if [ $rc -ne 0 ] || [ $rv -ne 0 ]
+then
+   printf "\nOR Tests completed. ERROR(s) encountered in %s non-critical command line test(s) and %s unit test(s).\n" $rv $rc
+   TEST_CLEANUP 1
+else
+    printf "\nOR Tests successfully executed.\n"
+fi
+
+TEST_CLEANUP $rv
+
